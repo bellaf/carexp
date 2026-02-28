884b56ebcfd362117a88cee8523a56cc862569e9
Tony Bell <tony@bellaf.plus.com>
2026-02-28 07:19:08 +0000

    Refine signed-out landing and logout flow

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,8 @@
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response->assertOk()
+        ->assertSee('Car Expense Tracker')
+        ->assertSee('Track the real cost of running your car.')
+        ->assertSee('Log In');
 });
