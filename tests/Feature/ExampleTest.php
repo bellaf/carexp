7a0274c96c55e066f65a17343c50a3a1675e557a
Tony Bell <tony@bellaf.plus.com>
2026-03-01 09:43:12 +0000

    Add iPhone web app metadata

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,5 +6,8 @@ test('returns a successful response', function () {
     $response->assertOk()
         ->assertSee('Car Expense Tracker')
         ->assertSee('Track the real cost of running your car.')
-        ->assertSee('Log In');
+        ->assertSee('Log In')
+        ->assertSee('rel="apple-touch-icon" href="/apple-touch-icon.png"', false)
+        ->assertSee('name="apple-mobile-web-app-capable" content="yes"', false)
+        ->assertSee('name="apple-mobile-web-app-title"', false);
 });
