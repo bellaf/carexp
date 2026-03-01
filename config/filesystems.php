6960021fd42faaf3810fb7cb3da72539b58a7d8e
Tony Bell <tony@bellaf.plus.com>
2026-03-01 19:54:51 +0000

    Document backup configuration options

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,13 @@ return [
             'report' => false,
         ],
 
+        'nas_backups' => [
+            'driver' => 'local',
+            'root' => env('BACKUP_NAS_PATH', '/mnt/synology-backups'),
+            'throw' => false,
+            'report' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
