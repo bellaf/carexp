273fe035d98f16e2b00405b816472ae31079c2c4
Tony Bell <tony@bellaf.plus.com>
2026-03-12 17:02:21 +0000

    Remove PHP upload diagnostics route

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/php-upload-check', function () {
-    return [
-        'sapi' => php_sapi_name(),
-        'loaded_ini' => php_ini_loaded_file(),
-        'upload_max_filesize' => ini_get('upload_max_filesize'),
-        'post_max_size' => ini_get('post_max_size'),
-        'memory_limit' => ini_get('memory_limit'),
-    ];
-});
-
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
